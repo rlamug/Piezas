be7f7da4eeb54fe4485f8d8bf9e0e1b43d86aa8a
rlamug <rlamug@mail.csuchico.edu>
2019-12-13 18:57:50 -0800

    Updated resetFilled test

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -21,9 +21,3 @@ TEST(PiezasTest, resetFilled) {
   piezas.dropPiece(0);
   ASSERT_EQ(piezas.pieceAt(0, 0), 'X');
 }
-TEST(PiezasTest, resetDropped) {
-  Piezas piezas;
-  piezas.dropPiece(0);
-  piezas.reset();
-  ASSERT_EQ(piezas.pieceAt(0, 0), ' ');
-}
