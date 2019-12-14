e80580f82f074681a3d9c59b02b450ad0a5f7b5a
rlamug <rlamug@mail.csuchico.edu>
2019-12-13 18:10:00 -0800

    Added reset test

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -1,20 +1,28 @@
 /**
  * Unit Tests for Piezas
-**/
+ **/
 
 #include <gtest/gtest.h>
 #include "Piezas.h"
- 
-class PiezasTest : public ::testing::Test
-{
-	protected:
-		PiezasTest(){} //constructor runs before each test
-		virtual ~PiezasTest(){} //destructor cleans up after tests
-		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor) 
+
+class PiezasTest : public ::testing::Test {
+ protected:
+  PiezasTest() {}             // constructor runs before each test
+  virtual ~PiezasTest() {}    // destructor cleans up after tests
+  virtual void SetUp() {}     // sets up before each test (after constructor)
+  virtual void TearDown() {}  // clean up after each test, (before destructor)
 };
 
-TEST(PiezasTest, sanityCheck)
-{
-	ASSERT_TRUE(true);
+// Sanity check
+TEST(PiezasTest, sanityCheck) { ASSERT_TRUE(true); }
+/*** reset ***/
+TEST(PiezasTest, reset) {
+  Piezas piezas;
+  for (int row = 0; row < BOARD_ROWS; row++) {
+    for (int column = 0; column < BOARD_COLS; column++) {
+      piezas.board[row][column] = X;
+    }
+  }
+  piezas.reset();
+  ASSERT_EQ(board[0][0], 'Blank');
 }
